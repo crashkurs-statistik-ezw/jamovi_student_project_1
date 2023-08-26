6b3b96e5a4eeb5c63a47dd1564557b213c5063e3
Christian <burkhart.christian@gmail.com>
2023-08-26 13:45:58 +0200

    init project

---
+++ data/data_quality.R
@@ -0,0 +1,14 @@
+library(assertr)
+library(tidyverse)
+
+# Gereinigter Datensatz laden ---------------------------------------------
+dframe <- read_csv("TODO") 
+
+
+# Daten einsehen ----------------------------------------------------------
+glimpse(dframe)
+view(dframe)
+
+# Data Quality Checks ausfuehren -------------------------------------------
+dframe
+
